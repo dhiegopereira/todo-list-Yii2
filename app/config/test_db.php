93bb6d8235941f3cbf48de188cdda5699db37fa4
studentdeveloperone <dhiego_ti@hotmail.com>
2024-08-19 13:05:44 -0300

    added test pipeline

--- app/config/test_db.php
+++ app/config/test_db.php
@@ -1,9 +1,5 @@
 <?php
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=db;dbname=yii2db';
-$db['username'] = 'yii2user';
-$db['password'] = 'yii2password';
-$db['charset'] = 'utf8';
 
 return $db;
